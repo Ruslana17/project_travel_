5b6b8e1d9dc472aef15fd66cc582d0e83e02bd91
RuslanaI7 <ruslana.fankovskaya@gmail.com>
2023-06-28 16:24:09 +0200

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+const loginPopup = document.querySelector('.button_login');
+const popup = document.querySelector('.login_popup');
+const searchInput = document.querySelector('.start_search_planning');
+
+loginPopup.addEventListener('click', ()=>{
+    popup.classList.toggle('flex')
+    searchInput.classList.toggle('hidden')
+})
+
+popup.addEventListener('click', (event)=>{
+  if(event.target.classList.contains('login_popup', 'flex',)){
+        popup.classList.toggle('display_none') 
+        searchInput.classList.toggle('visible')
+    }
+  /*console.log(event)*/
+})
